f4e8c663a3e0374c53f3f065f5e5b6da0954921d
unknown <etonloupe@yahoo.com.au>
2019-10-19 11:17:37 +1100

    initial commit

---
+++ Server/Server/Controllers/BillController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Server.Controllers
+{
+    [ApiController]
+    [Route("bill")]
+    public class BillController : ControllerBase
+    {
+        private readonly ILogger<BillController> _logger;
+        private static readonly ConcurrentDictionary<Guid, ItemLine> _itemLines = new ConcurrentDictionary<Guid, ItemLine>();
+
+        public BillController(ILogger<BillController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("billId")]
+        public IEnumerable<ItemLine> Get(Guid billId)
+        {
+            _logger.LogDebug("Getting ItemLines for billId: " + billId);
+            return _itemLines.Values.Where(x => x.BillId == billId);
+        }
+    }
+}
---
+++ Server/Server/ItemLine.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Server
+{
+    public class ItemLine
+    {
+        internal const int YES = 1;
+        internal const int NO = 0;
+
+        public Guid BillId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int IsClaimed { get; set; }
+        public Guid ClaimerId { get; set; }
+        public bool IsPaid { get; set; }
+        public Guid PayerId { get; set; }
+        public decimal Tip { get; set; }
+    }
+}
